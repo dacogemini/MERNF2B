4e69bcc9a27168269a9c14e8416bfd50034076ee
Dan Collins <32271799+dacogemini@users.noreply.github.com>
2018-09-12 14:49:33 -0500

    Changed model, added bcrypt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 // Deals with authentication
-
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+// Todo | Encrypt password
+const bcrypt = require('bcryptjs');
 
 // Load User model
 const User = require('../../models/User');
@@ -22,9 +24,9 @@ router.get('/test', (req, res) => res.json({
 //* @access PUBLIC << Note: in order to access a private route, you'll need a JSON Web Token
 
 router.post('/register', (req, res) => {
-        //todo | Use mongoose to find if email exists
+        //todo | Use mongoose method (findOne) to find if email exists
         User.findOne({
-                email: req.body.email
+                email: req.body.email //* Find an email that matches req.body.email
             }) //! << uses body-parser
             // Promise
             // A promise is a placeholder 
@@ -36,12 +38,26 @@ router.post('/register', (req, res) => {
                         email: 'Email already exists'
                     });
                 } else {
+                    const avatar = gravatar.url(req.body.email, {
+                        s: '200', // Size
+                        r: 'pg', // Rating
+                        d: 'mm' // Default
+                    });
                     const newUser = new User({
                         name: req.body.name,
                         email: req.body.email,
                         avatar,
                         password: req.body.password
                     });
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if(err) throw err;
+                            newUser.password = hash;
+                            newUser.save()
+                                .then(user => res.json(user))
+                                .catch(err => console.log(err));
+                        });
+                    })                    
                 }
             })
     }),
